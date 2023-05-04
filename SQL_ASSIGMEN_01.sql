e695d6fb3b1bacda02c356e3210eace3753930b4
GMyracle <95431165+GMyracle@users.noreply.github.com>
2023-05-04 13:16:22 +0700

    Update SQL_ASSIGMEN_01.sql
    ASSIGMENT1

--- SQL_ASSIGMEN_01.sql
+++ SQL_ASSIGMEN_01.sql
@@ -19,7 +19,9 @@ Username       varchar(50)unique key not null,
 Fullname       varchar(50) not null,
 Department_id         tinyint unsigned not null,
 Position_id    tinyint unsigned not null,
-CreateDate     datetime 
+CreateDate     datetime ,
+foreign key (Department_id) references Department(Department_id),
+foreign key (Position_id) references `Position` (Position_id)
 );
 
 
@@ -34,7 +36,9 @@ Create_date       datetime
 CREATE TABLE IF NOT EXISTS GroupAccount(
 Group_id       tinyint unsigned not null primary key,
 Account_id         tinyint unsigned not null,
-Join_date      datetime 
+Join_date      datetime ,
+foreign key (Group_id) references `Group`(Group_id),
+foreign key (Account_id) references `Account`(Account_id)
 );
 
 
@@ -56,7 +60,9 @@ Content              varchar(50) unique key not null,
 CateQuestion_id      tinyint unsigned not null,
 Type_id              tinyint unsigned not null ,
 Creator_id           tinyint unsigned not null ,
-Create_date          datetime
+Create_date          datetime,
+foreign key (CateQuestion_id) references CategoryQuestion(CateQuestion_id),
+foreign key (Type_id) references TypeQuestion(Type_id)
 );
 
 
@@ -64,7 +70,8 @@ CREATE TABLE IF NOT EXISTS Answer(
 ans_id           tinyint unsigned auto_increment primary key,
 Content          varchar(50) unique key not null,
 quest_id         tinyint unsigned not null, 
-is_correct       enum('failse','true') unique key not null
+is_correct       enum('failse','true') unique key not null,
+foreign key (quest_id) references Questions(quest_id)
 );
 
 CREATE TABLE IF NOT EXISTS Exam(
@@ -74,11 +81,15 @@ title             varchar(50) not null,
 CateQuestion_id   tinyint unsigned not null,
 duration          date not null,
 Creator_id        tinyint unsigned not null,
-Create_date       datetime 
+Create_date       datetime ,
+foreign key (cateQuestion_id) references CategoryQuestion(CateQuestion_id),
+foreign key (Creator_id) references `Account`(Account_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS ExamQuestion(
 exam_id       tinyint unsigned not null primary key,
-quest_id      tinyint unsigned not null
+quest_id      tinyint unsigned not null,
+foreign key (exam_id) references Exam (exam_id),
+foreign key (quest_id) references Questions(quest_id)
 );
